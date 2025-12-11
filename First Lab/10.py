14acd1f2c0cf132d1a034ab548714a9c3e5ef2df
Nabin Timsina <timsinanabin8@gmail.com>
2025-12-11 09:14:34 +0545

    2nd

---
+++ First Lab/10.py
@@ -0,0 +1,21 @@
+import math
+
+def rotate_coordinate(x, y, degrees):
+  
+    radians = math.radians(degrees)
+    cos_theta = math.cos(radians)
+    sin_theta = math.sin(radians)
+
+    x_rot = x * cos_theta - y * sin_theta
+    y_rot = x * sin_theta + y * cos_theta
+
+    return (x_rot, y_rot)
+
+    
+original_x, original_y = 10, 0
+angle = 90
+new_x, new_y = rotate_coordinate(original_x, original_y, angle)
+
+print(f"Original: ({original_x}, {original_y})")
+print(f"Rotated: ({new_x}, {new_y})")
+    
--- First Lab/9.py
+++ First Lab/9.py
@@ -6,6 +6,6 @@ x2 = int(input("enter x2 "))
 y2 = int(input("enter y2 "))
 
 
-dis = math.sqrt((x2-x1)**2 + (y2- y1)**2)
+dis = math.sqrt(pow((x2-x1), 2) + pow((y2- y1), 2))
 
 print(dis)
