de61251e2337802e7f6492c5e3eb820762497e38
Nabin Timsina <timsinanabin8@gmail.com>
2026-01-29 07:13:41 +0545

    bird image missing

--- Project/main.py
+++ Project/main.py
@@ -1,1 +1,21 @@
+import sys
 import pygame
+
+from settings import SCREEN_WIDTH, SCREEN_HEIGHT
+from game import Game
+
+
+def main() -> None:
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Flappy Bird")
+
+    game = Game(screen)
+    game.run()
+
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
